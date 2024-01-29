7e7465fce8affa48006c07a5b7a7fd44dde28032
Pratika1012 <118338761+Pratika1012@users.noreply.github.com>
2024-01-29 10:37:36 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -18,48 +19,51 @@ st.subheader(':red[HUMANS]')
 # Add a subheader for "Burns"
 st.markdown("<h4>Burns</h4>",unsafe_allow_html=True)
 
-# List of burn scenarios
-burn_scenarios = [
-    "Burned Arm by hot milk",
-    "Burned hand by hot oil",
-    "Burned arm by steam car radiator",
-    "Third degree Burn from walking on hot stones in temple",
-    "Other"
-]
+data=pd.read_excel("Case Studies.xlsx")
+data.columns = data.iloc[0]
+data = data[1:]
+# # List of burn scenarios
+# burn_scenarios = [
+#     "Burned Arm by hot milk",
+#     "Burned hand by hot oil",
+#     "Burned arm by steam car radiator",
+#     "Third degree Burn from walking on hot stones in temple",
+#     "Other"
+# ]
 
-# Display the list as clickable hyperlinks
+# # Display the list as clickable hyperlinks
 
-for scenario in burn_scenarios:
-    st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
-
-# Display details for each burn scenario
 # for scenario in burn_scenarios:
-#     st.markdown(f"### {scenario.replace(' ', '-').lower()}")
-#     st.write(f"Details for {scenario} go here.")
-st.markdown("<h4>Ulcers</h4>",unsafe_allow_html=True)
-ulcers_scenarios=[
-    "Diabetic Ulcer",
-    "Diabetic Ulcer suffering from ore than a Year",
-    "Venous Ulcer",
-] 
+#     st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
 
-for scenario in ulcers_scenarios:
-    st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
+# # Display details for each burn scenario
+# # for scenario in burn_scenarios:
+# #     st.markdown(f"### {scenario.replace(' ', '-').lower()}")
+# #     st.write(f"Details for {scenario} go here.")
+# st.markdown("<h4>Ulcers</h4>",unsafe_allow_html=True)
+# ulcers_scenarios=[
+#     "Diabetic Ulcer",
+#     "Diabetic Ulcer suffering from ore than a Year",
+#     "Venous Ulcer",
+# ] 
+
+# for scenario in ulcers_scenarios:
+#     st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
 
     
-st.markdown("<h4>Others</h4>",unsafe_allow_html=True)
-others_scenarios=[
-    "Diabetic Bedsore",
-    "gangreme",
-    "Accident with industrial sugar came crusher",
-    "Maggot infection"
-] 
-for scenario in others_scenarios:
-    st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
+# st.markdown("<h4>Others</h4>",unsafe_allow_html=True)
+# others_scenarios=[
+#     "Diabetic Bedsore",
+#     "gangreme",
+#     "Accident with industrial sugar came crusher",
+#     "Maggot infection"
+# ] 
+# for scenario in others_scenarios:
+#     st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
 
-st.subheader(':red[ANIMALS]')
-Animals_scenarios=[
-    "Jumping horse wounded with tractor Fork"
-]
-for scenario in Animals_scenarios:
-    st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
+# st.subheader(':red[ANIMALS]')
+# Animals_scenarios=[
+#     "Jumping horse wounded with tractor Fork"
+# ]
+# for scenario in Animals_scenarios:
+#     st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
