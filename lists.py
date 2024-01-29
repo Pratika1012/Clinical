2279d64856d9bd1d72d4f431924a0e09daf54ab6
Pratika1012 <118338761+Pratika1012@users.noreply.github.com>
2024-01-29 11:29:23 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -4,8 +4,7 @@ st.set_page_config(layout="wide")
 
 st.markdown("""
  <style>
- .block-container {
- padding-top: 1rem;
+ .block-container {padding-top: 1rem;
  padding-bottom: 3rem;
  padding-right: 5rem;
  }
@@ -19,49 +18,32 @@ st.subheader(':red[HUMANS]')
 st.markdown("<h4>Burns</h4>",unsafe_allow_html=True)
 
 # List of burn scenarios
-burn_scenarios = [
-    "Burned Arm by hot milk",
-    "Burned hand by hot oil",
-    "Burned arm by steam car radiator",
-    "Third degree Burn from walking on hot stones in temple",
-    "Burns caused by hot tea"
-]
+st.write("*  [Burned arm by hot milk](https://drive.google.com/file/d/1ykzll03BUiY0uO2oB1bJcCsb2wfTDmKo/view?usp=drive_link)")
+st.write("*  [Burned hand by hot oil](https://drive.google.com/file/d/1KQloOjlHgQaFVetH6ppAelA_569K21Lu/view?usp=drive_link)")
+st.write("*  [Burned arm by steam car radiator](https://drive.google.com/file/d/1UbebKHuo7KvqTcSvRzCJJynKoAxGz0Ky/view?usp=drive_link)")
+st.write("*  [Third degree burn from walking on hot stones in temple](https://drive.google.com/file/d/1KYFVCY2nmS8C-Li_GEnN3rZsaG0Oa8WU/view?usp=drive_link)")
+st.write("*  [Burns caused by hot tea](https://drive.google.com/file/d/1KYFVCY2nmS8C-Li_GEnN3rZsaG0Oa8WU/view?usp=drive_link)")
 
-# Display the list as clickable hyperlinks
 
-for scenario in burn_scenarios:
-    st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
-
-# Display details for each burn scenario
-# for scenario in burn_scenarios:
-#     st.markdown(f"### {scenario.replace(' ', '-').lower()}")
-#     st.write(f"Details for {scenario} go here.")
 st.markdown("<h4>Ulcers</h4>",unsafe_allow_html=True)
-ulcers_scenarios=[
-    "Diabetic Ulcer",
-    "Diabetic Ulcer suffering from ore than a Year",
-    "Venous Ulcer",
-] 
 
-for scenario in ulcers_scenarios:
-    pdf_link = f"[Download PDF](https://drive.google.com/file/d/1mRxcnRi0WIYLb_P7tHfJf9kQXIyayyxR/view?usp=drive_link{scenario.replace(' ', '-').lower()}.pdf)"
-    st.markdown(f"- [{scenario}]({pdf_link}) (#{scenario.replace(' ', '-').lower()})")
-    
+st.write("*  [Diabetic Ulcer](https://drive.google.com/file/d/1mRxcnRi0WIYLb_P7tHfJf9kQXIyayyxR/view?usp=drive_link)")
+st.write("*  [Diabetic Ulcer](https://drive.google.com/file/d/1aPBCI2IS7kf_WID9jhHa5JL4YY1etBDQ/view?usp=drive_link)")
+st.write("*  [Venous ulcer](https://drive.google.com/file/d/15QIOQI75Rg7utbU_txj65_HoItyk577b/view?usp=drive_link)")
 
-    
-st.markdown("<h4>Others</h4>",unsafe_allow_html=True)
-others_scenarios=[
-    "Diabetic Bedsore",
-    "gangreme",
-    "Accident with industrial sugar came crusher",
-    "Maggot infection"
-] 
-for scenario in others_scenarios:
-    st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
+
+st.markdown("<h4>Others</h4>",unsafe_allow_html=True)  
+st.write("*  [ Diabetic Bedsore](https://drive.google.com/file/d/1zmb2KcWx3EUtlmEFg6lnYIGH58r7TYWr/view?usp=drive_link)")
+st.write("*  [Gangreme](https://drive.google.com/file/d/1-9lpKWHe0aYUh7bJyCmWhg-I-c8jJiMq/view?usp=drive_link)")
+st.write("*  [Accident with industrial sugar cane crusher](https://drive.google.com/file/d/1ZS-QuaQeaAMN3cTLBIVaVPJbKHaZkGYM/view?usp=drive_link)")
+st.write("*  [Maggot infection](https://drive.google.com/file/d/1UpboXUcKa5yxpQABeqPTDOpOIWwqxsHJ/view?usp=drive_link)")
+  
+
 
 st.subheader(':red[ANIMAL]')
-Animals_scenarios=[
-    "Jumping horse wounded by tractor fork"
-]
-for scenario in Animals_scenarios:
-    st.markdown(f"- [{scenario}](#{scenario.replace(' ', '-').lower()})")
+
+st.write("* [Jumping horse wounded by tractor fork](https://drive.google.com/file/d/1gpiHbEZIhzg1PwAGkN9LeZG1mPXYO1wl/view?usp=sharing)")
+
+
+
+
